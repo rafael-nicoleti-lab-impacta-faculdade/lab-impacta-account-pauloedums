2fe6bde1371eb5d2b597e95a3cfe4d4ded31f0e0
Paulo Marques <pauloedums@gmail.com>
2021-10-30 12:56:26 -0300

    Adicionando camadas da aplicação

---
+++ account/src/main/java/br/com/lab/impacta/account/service/AccountService.java
@@ -0,0 +1,9 @@
+package br.com.lab.impacta.account.service;
+
+import br.com.lab.impacta.account.model.Account;
+
+public interface AccountService {
+    Account findAccount(Long accountId);
+
+    void debitAccount(Long accountId, Double valueOfDebit);
+}
